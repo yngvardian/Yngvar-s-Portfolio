2de929971fb91e781c42baaedbf9c32401c26dbe
yngvardian <102372323+yngvardian@users.noreply.github.com>
2022-09-10 20:06:17 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+    myFunction()
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -9,9 +11,50 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+
+    window.addEventListener('scroll', animOnScroll);
+
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+
+            } else {
+                animItem.classList.remove('_active');
+            }
+        }
+    }
+
+    function offset(el) {
+
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pagexOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageyOffset || document.documentElement.scrollTop;
+        return {
+            top: rect.top + scrollTop,
+            left: rect.left + scrollLeft
+        }
+    }
+    animOnScroll();
 }
